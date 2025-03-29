0cbd2b3e26d8a336b018f3b74ed35336c3daa699
Henrick Silva <henrick040919@gmail.com>
2025-03-29 18:24:56 -0300

    Added footer and updated visuals

--- footer.php
+++ footer.php
@@ -1,9 +1,17 @@
-</div>
+    </main>
 
+    <footer class="mt-5 p-4 d-flex justify-content-between align-items-center text-center text-white">
+        <span>Termos|Políticas</span>
+        <div class="d-flex align-items-center">
+            <span>© Copyright 2023 | Desenvolvido por</span>
+            <img src="public/assets/logo_rodape_alphacode.png" alt="rodapeLogo" id="rodapeLogo">
+        </div>
+        <span>©Alphacode IT Solutions 2023</span>
+    </footer>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-</script>
-</body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+    </body>
 
-</html>
+    </html>
--- index.php
+++ index.php
@@ -1,7 +1,62 @@
 <?php include('header.php') ?>
 <?php include('database_connection.php') ?>
 
-<table class="table table-hover">
+
+<form class="row mb-5" id="form">
+    <div class="form-group col-md-6">
+        <label for="inputName">Nome completo</label>
+        <input type="text" class="form-control rounded-0 p-0 mb-5" id="inputName" name="name" placeholder="Ex.: Letícia Pacheco dos Santos" maxlength="200" required>
+    </div>
+
+    <div class="form-group col-md-6">
+        <label for="inputBirthDate">Data de nascimento</label>
+        <input type="text" class="form-control rounded-0 p-0 mb-5 date" id="inputBirthDate" name="birth_date" placeholder="Ex.: 03/10/2003" data-inputmask="'alias': 'date'" required>
+    </div>
+
+    <div class="form-group col-md-6">
+        <label for="inputEmail">E-mail</label>
+        <input type="email" class="form-control rounded-0 p-0 mb-5" id="inputEmail" name="email" placeholder="Ex.: leticia@gmail.com" maxlength="200" required>
+    </div>
+
+    <div class="form-group col-md-6">
+        <label for="inputOccupation">Profissão</label>
+        <input type="text" class="form-control rounded-0 p-0 mb-5" id="inputOccupation" name="occupation" placeholder="Ex.: Desenvolvedora Web" maxlength="200" required>
+    </div>
+
+    <div class="form-group col-md-6">
+        <label for="inputPhone">Telefone para contato</label>
+        <input type="text" class="form-control rounded-0 p-0 mb-5" id="inputPhone" name="phone" placeholder="Ex.: (11) 4033-2019">
+    </div>
+
+    <div class="form-group col-md-6">
+        <label for="inputMobilePhone">Celular para contato</label>
+        <input type="text" class="form-control rounded-0 p-0 mb-5" id="inputMobilePhone" name="mobile_phone" placeholder="Ex.: (11) 98493-2039" required>
+    </div>
+
+    <div class="form-group col-md-6">
+        <input type="checkbox" class="form-check-input" id="checkboxWhatsapp" name="is_whatsapp" checked />
+        <label class="form-check-label mb-4" for="checkboxWhatsapp">Número de celular possui Whatsapp</label>
+    </div>
+
+    <div class="form-group col-md-6">
+        <input type="checkbox" class="form-check-input" id="checkboxEmail" name="receive_email" checked />
+        <label class="form-check-label mb-4" for="checkboxEmail">Enviar notificações por E-mail</label>
+    </div>
+
+    <div class="form-group col-md-6">
+        <input type="checkbox" class="form-check-input" id="checkboxSms" name="receive_sms" checked />
+        <label class="form-check-label mb-4" for="checkboxSms">Enviar notificações por SMS</label>
+    </div>
+
+    <div class="d-flex justify-content-end">
+        <button class="btn btn-primary" type="submit" name="register">Cadastrar contato</button>
+    </div>
+</form>
+
+
+<hr><br>
+
+<table class="table table-hover mt-5">
     <thead id="data_section_top">
         <tr>
             <th>Nome</th>
@@ -11,63 +66,6 @@
             <th>Ações</th>
         </tr>
     </thead>
-    <form class="form-container mt-5 mb-5">
-        <div class="row">
-            <div class="col-12 col-md-6">
-                <div class="form-group">
-                    <label for="inputName">Nome completo</label>
-                    <input type="text" class="form-control form-control-lg" id="inputName" name="name" placeholder="Ex.: Letícia Pacheco dos Santos" maxlength="200" required>
-                </div>
-
-                <div class="form-group">
-                    <label for="inputEmail">E-mail</label>
-                    <input type="email" class="form-control form-control-lg" id="inputEmail" name="email" placeholder="Ex.: leticia@gmail.com" maxlength="200" required>
-                </div>
-
-                <div class="form-group">
-                    <label for="inputPhone">Telefone para contato</label>
-                    <input type="text" class="form-control form-control-lg" id="inputPhone" name="phone" placeholder="Ex.: (11) 4033-2019">
-                </div>
-
-                <div class="form-group">
-                    <input type="checkbox" class="form-check-input" id="checkboxWhatsapp" name="is_whatsapp" checked />
-                    <label class="form-check-label" for="checkboxWhatsapp">Número de celular possui Whatsapp</label>
-                </div>
-
-                <div class="form-group">
-                    <input type="checkbox" class="form-check-input" id="checkboxEmail" name="receive_email" checked />
-                    <label class="form-check-label" for="checkboxEmail">Enviar notificações por E-mail</label>
-                </div>
-            </div>
-
-            <div class="col-12 col-md-6">
-                <div class="form-group">
-                    <label for="inputBirthDate">Data de nascimento</label>
-                    <input type="text" class="form-control date form-control-lg" id="inputBirthDate" name="birth_date" placeholder="Ex.: 03/10/2003" data-inputmask="'alias': 'date'" required>
-                </div>
-
-                <div class="form-group">
-                    <label for="inputOccupation">Profissão</label>
-                    <input type="text" class="form-control form-control-lg" id="inputOccupation" name="occupation" placeholder="Ex.: Desenvolvedora Web" maxlength="200" required>
-                </div>
-
-                <div class="form-group">
-                    <label for="inputMobilePhone">Celular para contato</label>
-                    <input type="text" class="form-control form-control-lg" id="inputMobilePhone" name="mobile_phone" placeholder="Ex.: (11) 98493-2039" required>
-                </div>
-
-                <div class="form-group">
-                    <input type="checkbox" class="form-check-input" id="checkboxSms" name="receive_sms" checked />
-                    <label class="form-check-label" for="checkboxSms">Enviar notificações por SMS</label>
-                </div>
-            </div>
-        </div>
-
-        <div class="d-flex justify-content-end">
-            <button class="btn btn-primary" type="submit" name="register">Cadastrar contato</button>
-        </div>
-    </form>
-    <hr>
     <?php
 
     $query = 'SELECT * FROM contact';
@@ -81,12 +79,15 @@
     } else {
         while ($row = mysqli_fetch_assoc($result)) {
     ?>
-            <tr>
-                <td><?php echo $row['full_Name'] ?></td>
-                <td><?php echo date('d/m/Y', strtotime($row['birth_Date'])) ?></td>
-                <td><?php echo $row['email'] ?></td>
-                <td><?php echo $row['mobile_Phone'] ?></td>
-                <td><img src="public/assets/editar.png" alt="edit_ast"></s><img src="public/assets/excluir.png" alt="delete.asset"></td>
+            <tr class="table_data mt-5 mb-5 p-5">
+                <td class="text-center"><?php echo $row['full_Name'] ?></td>
+                <td class="text-center"><?php echo date('d/m/Y', strtotime($row['birth_Date'])) ?></td>
+                <td class="text-center"><?php echo $row['email'] ?></td>
+                <td class="text-center"><?php echo $row['mobile_Phone'] ?></td>
+                <td class="text-center">
+                    <img src="public/assets/editar.png" alt="edit_ast" class="icon">
+                    <img src="public/assets/excluir.png" alt="delete.asset">
+                </td>
             </tr>
 
     <?php
@@ -95,5 +96,6 @@
     ?>
     </tbody>
 </table>
+</div>
 
 <?php include('footer.php') ?>
